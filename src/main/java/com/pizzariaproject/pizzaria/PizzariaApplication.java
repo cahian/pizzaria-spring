9d02d988c5c348e3225cc548c253e97e4f4d37e0
Cahian Freire <cahianfreire@gmail.com>
2023-10-19 15:26:19 -0300

    Add initial code

--- src/main/java/com/pizzariaproject/pizzaria/PizzariaApplication.java
+++ src/main/java/com/pizzariaproject/pizzaria/PizzariaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzariaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PizzariaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PizzariaApplication.class, args);
+    }
 }
